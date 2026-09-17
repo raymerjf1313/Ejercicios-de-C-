bd336759ff3dd8fdb633a3a2b782a959ad4a9960
Raymer <2025-0009@unicda.edu.do>
2026-09-16 20:36:27 -0400

    Ejercicio 7 - Jedlik's Toys

---
+++ Ejercicio7/Ejercicio7.cs
@@ -0,0 +1,39 @@
+﻿public class RemoteControlCar
+{
+    private int _distanciaMetros = 0;
+    private int _porcentajeBateria = 100;
+
+    public static RemoteControlCar Buy()
+    {
+        return new RemoteControlCar();
+    }
+
+    public string DistanceDisplay()
+    {
+        return $"Driven {_distanciaMetros} meters";
+    }
+
+    public string BatteryDisplay()
+    {
+        if (_porcentajeBateria == 0)
+            return "Battery empty";
+        
+        return $"Battery at {_porcentajeBateria}%";
+    }
+
+    public void Drive()
+    {
+        if (_porcentajeBateria > 0)
+        {
+            _distanciaMetros += 20;
+            _porcentajeBateria -= 1;
+        }
+    }
+
+    public static void Main()
+    {
+        var coche = RemoteControlCar.Buy();
+        Console.WriteLine(coche.DistanceDisplay());
+        Console.WriteLine(coche.BatteryDisplay());
+    }
+}
